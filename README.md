aa58b9632c0c4578772ce907510fd5a5cfbe3ab3
Alisher Tortay <t.b.alisher@gmail.com>
2018-05-26 18:45:48 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Add/Edit javascript files in javascript directory.
+
+You don't need to change files (they are there for reference) in following directories: assets, templates
+
+We will delete the left panel after we finish implementation.
---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by alisher on 5/26/18.
+ */
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyAvVTgyZW_IpBkNyWKDUFnJjNKlDlcl7Bo",
+authDomain: "kaist-scms.firebaseapp.com",
+databaseURL: "https://kaist-scms.firebaseio.com",
+projectId: "kaist-scms",
+storageBucket: "",
+messagingSenderId: "716217260337"
+};
+firebase.initializeApp(config);
